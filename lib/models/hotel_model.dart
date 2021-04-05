a8660815b07f3df8c07eade028765f4e482946fd
Aspendas <69016761+Aspendas@users.noreply.github.com>
2021-04-05 21:01:44 +0300

    Hotel model changed

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -2,12 +2,17 @@ class Hotel {
   String imageUrl;
   String name;
   String address;
+  String description;
+  double rate;
   int price;
+
 
   Hotel({
     this.imageUrl,
     this.name,
     this.address,
+    this.description,
+    this.rate,
     this.price,
   });
 }
@@ -15,20 +20,26 @@ class Hotel {
 final List<Hotel> hotels = [
   Hotel(
     imageUrl: 'assets/images/hotel0.jpg',
-    name: 'Hotel 0',
-    address: '404 Great St',
+    name: 'Hotel Diana',
+    address: 'İzmir, Turkey',
+    description: "Amazing hotel at Izmir, Turkey have a good swimming pool.",
+    rate: 4.4,
     price: 175,
   ),
   Hotel(
     imageUrl: 'assets/images/hotel1.jpg',
-    name: 'Hotel 1',
-    address: '404 Great St',
+    name: 'Hotel Miami',
+    address: 'Miami, Florida, USA',
+    description: "Amazing hotel at Miami, USA near the ocean.",
+    rate: 4.3,
     price: 300,
   ),
   Hotel(
     imageUrl: 'assets/images/hotel2.jpg',
-    name: 'Hotel 2',
-    address: '404 Great St',
+    name: 'Hotel Bali',
+    address: 'Bali, Indonesia',
+    description: "Amazing hotel at Bali, Indonesia really cheap and exotic.",
+    rate: 4.8,
     price: 240,
   ),
 ];
